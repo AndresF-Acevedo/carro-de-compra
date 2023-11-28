b144559c3568ce104b0eda728bab6aeabbfdc22f
Andres Acevedo <andresomg333@gmial.com>
2023-11-28 16:08:58 -0500

    feat create buycar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { Component } from "react"
 import Productos from "./components/Productos"
+import Layout from "./components/Layout"
+import Navbar from "./components/Navbar"
+import Title from "./components/Title"
 
 class App extends Component{
     state = {
@@ -7,17 +10,46 @@ class App extends Component{
             { name: "tomate", price:1500, img: "/productos/tomate.jpg"},
             { name: "arbeja", price:2500, img: "/productos/arbejas.jpg"},
             { name: "lechuga", price:1000, img: "/productos/lechuga.jpg"},
-        ]
+        ],
+        carro: [],
     }
-    render(){
-        return(
+
+    agregarAlCarro = (producto) => {
+        const { carro } =this.state
+        if (carro.find(x => x.name === producto.name)){
+            const newCarro = carro.map(x => x.name === producto.name
+            ? ({
+                    ...x,
+                    cantidad: x.cantidad +1
+
+                })
+            :x)
+            return this.setState({carro: newCarro})
+        }
+        return this.setState({
+            carro:this.state.carro.concat({
+                ...producto,
+                cantidad:1,
+            })
+            }
+
+        )
+
+    }
+    render() {
+        return (
             <div>
-                <Productos
-                    productos={this.state.productos}
-                    agregarAlCarro={() => console.log("nada")}
-                />
+                <Navbar carro={this.state.carro}> </Navbar>
+                <Layout>
+                    <Title  />
+                    <Productos
+                        productos={this.state.productos}
+                        agregarAlCarro={this.agregarAlCarro}
+                    />
+                </Layout>
             </div>
         )
     }
 }
+
 export default App;
