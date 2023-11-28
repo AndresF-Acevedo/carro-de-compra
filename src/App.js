75a9c5735d4b5fc614d0529a3ccc26ec9f358a47
Andres Acevedo <andresomg333@gmial.com>
2023-11-27 21:29:57 -0500

    update products project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react"
+import Productos from "./components/Productos"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    state = {
+        productos: [
+            { name: "tomate", price:1500, img: "/productos/tomate.jpg"},
+            { name: "arbeja", price:2500, img: "/productos/arbejas.jpg"},
+            { name: "lechuga", price:1000, img: "/productos/lechuga.jpg"},
+        ]
+    }
+    render(){
+        return(
+            <div>
+                <Productos
+                    productos={this.state.productos}
+                    agregarAlCarro={() => console.log("nada")}
+                />
+            </div>
+        )
+    }
 }
-
 export default App;
